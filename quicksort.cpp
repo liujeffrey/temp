7eaad7a801d24a5727f049c5ce5bfca062635630
liulei <liulei@yeah.net>
2015-11-03 22:41:39 +0800

    fix quicksort

--- quicksort.cpp
+++ quicksort.cpp
@@ -1,13 +1,14 @@
 #include<iostream>
 using namespace std;
 
+void quick_sort(int a[], int low, int high);
 int main() 
 {
   int data[9] = {3,89,22,4,66,89,1,20,12};
   quick_sort(data, 0, 8);
   for(int i=0;i<9;i++) 
   {
-    cout<<a[i]<<",";
+    cout<<data[i]<<",";
   }
   cout<<endl;
 }
@@ -15,25 +16,31 @@ int main()
 void quick_sort(int a[], int low, int high)
 {
   int i,j,pivok;
-  i = low;
-  j = high;
-  pivok = a[i];
   
-  while(i < j) 
-  { 
-    while(i < j && a[j] > pivok)
-	   j--;
-	if (i < j)
-	  a[i++] = a[j];
-	
-	while(i < j && a[i] < pivok)
-	  i++;
-	  
-	if (i < j)
-	  a[j] = a[i++];
+  
+  if (low < high)
+  {
+	 i = low;
+     j = high;
+     pivok = a[i]; 
+	 while(i < j) 
+     { 
+		while(i < j && a[j] > pivok)
+		   j--;
+		if (i < j)
+		  a[i++] = a[j];
+		
+		while(i < j && a[i] < pivok)
+		  i++;
+		  
+		if (i < j)
+		  a[j] = a[i++];
+     }
+	 a[i] = pivok;
+	 quick_sort(a, low, i - 1);
+	 quick_sort(b, i + 1, high);
   }
   
-  a[i] = pivok;
-  quick_sort(a, low, i - 1);
-  quick_sort(b, i + 1, high);
+  
+  
 }
